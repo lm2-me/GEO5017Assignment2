5915cefe6f929681e4cf42590e551f9f5196210c
Sebastian Stripp <69217147+Lundkjaer@users.noreply.github.com>
2022-03-14 14:30:52 +0100

    updates to multiple files, SVM and organising

--- Evaluation.py
+++ Evaluation.py
@@ -4,7 +4,7 @@
 # sklearn.metrics.accuracy_score(y_true, y_pred, *[, ...])
 
 # Mean per-class accuracy
-# sklearn.metrics.average_precision_score(y_true, ...)
+# sklearn.metrics.balanced_accuracy_score(y_true, ...)
 
 # Confusion Matrix
 # sklearn.metrics.confusion_matrix(y_true, y_pred, *)
