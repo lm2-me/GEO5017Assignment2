b0de06a79625b9e8679ab4188efb482ad63c94a3
yuyut <33997318+yuyut@users.noreply.github.com>
2022-03-21 18:25:46 +0100

    try to implement plot, but having trouble with data

--- A2.py
+++ A2.py
@@ -32,31 +32,19 @@ import RF
 
 #Main
 if __name__ == "__main__":
-    ##add back for final code before submitting
     y_true = np.loadtxt('y_true.csv', dtype='str', delimiter='\n')
 
-    # pointCloudDirectory = ft.importFiles()
-    # object_features = ft.allObjectProperties(pointCloudDirectory)
+    pointCloudDirectory = ft.importFiles()
+    object_features = np.array(ft.allObjectProperties(pointCloudDirectory))
+    features_only = ft.normalize_features(object_features)
+    print(features_only)
 
-    object_features = np.loadtxt('features_norm_new.csv', delimiter=',')
+    ##dataload = np.loadtxt('features_norm.csv', delimiter=',')
 
     ## = dataload[:,1:]
-
-    #Average Height, Squareness [2, 6]
-    object_features1 = object_features[:,[2,6]]
-    #Height, Average Height, Squareness [0, 2, 6]
-    object_features2 = object_features[:,[0,2,6]]
-    #Average Height, Planarity, Squareness [0, 5, 6]
-    object_features3 = object_features[:,[0,5,6]]
-    #Set of 3 [0, 0, 0]
-    object_features4 = object_features[:,[0,0,0]]
-    #Height, Average Height, Planarity, Squareness [0, 2, 5, 6]
-    object_features5 = object_features[:,[0,2,5,6]]
-    #Set of 5  [0, 0, 0, 0, 0]
-    object_features6 = object_features[:,[0,0,0,0,0]]
     
-    #Random Forest
-    X_train, X_test, y_train, y_test = RF.splitdata(object_features, y_true)
+    #####
+    X_train, X_test, y_train, y_test = RF.splitdata(features_only, y_true)
     y_predRF, y_testRF = RF.randomforest(X_train, X_test, y_train, y_test)
 
     rf_oa = ev.overallAccuracy(y_testRF, y_predRF)
@@ -67,17 +55,4 @@ if __name__ == "__main__":
     print('Random Forest: Mean Per-Class Accuracy', rf_mpa)
     print('Random Forest: Confusion Matrix \n', rf_cm)
 
-    #RF.learningcurve(object_features, y_true)
-
-    #SVM
-    X_trainSVM, X_testSVM, y_trainSVM, y_testSVM = SVM.splitdata(object_features5, y_true)
-    y_predSVM, y_testSVM = SVM.support_vector_machine(X_trainSVM, X_testSVM, y_trainSVM, y_testSVM)
-
-    svm_oa = ev.overallAccuracy(y_testSVM, y_predSVM)
-    svm_mpa = ev.meanPerClassAccuracy(y_testSVM, y_predSVM)
-    svm_cm = ev.confusionMatrix(y_testSVM, y_predSVM)
-
-    print('Support Vector Machine: Overall Accuracy', svm_oa)
-    print('Support Vector Machine: Mean Per-Class Accuracy', svm_mpa)
-    print('Support Vector Machine: Confusion Matrix \n', svm_cm)
 
--- RF.py
+++ RF.py
@@ -4,10 +4,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn import svm
-import math
-import scipy
-import Evaluation as ev
-
 
 
 
@@ -33,45 +29,48 @@ def randomforest(X_train, X_test, y_train, y_test):
 
     return y_pred, y_test
 
-def learningcurve(features_only, y_true):
-    X = features_only
-    y = y_true
-       
-    test_size_neg = []
-    test_size_record = []
-    AO = []
+def rf_Plot_max_depth(x_train, x_test, y_train, y_test):
+    max_depths = np.linspace(1, 32, 32, endpoint=True)
+    train_results = []
+    test_results = []
+    for max_depth in max_depths:
+        rf = RandomForestClassifier(max_depth=max_depth, n_jobs=-1)
+        rf.fit(x_train, y_train)
+        train_pred = rf.predict(x_train)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train, train_pred)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+        train_results.append(roc_auc)
+        y_pred = rf.predict(x_test)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+        test_results.append(roc_auc)
 
-    test_size = 0
-    while test_size < 0.99:
-        test_size += 0.01
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    line1, = plt.plot(max_depths, train_results, 'b', label ="Train AUC")
+    line2, = plt.plot(max_depths, test_results, 'r', label ="Test AUC")
+    plt.legend(handler_map={line1: HandlerLine2D(numpoints=2)})
+    plt.ylabel('AUC score')
+    plt.xlabel('Tree depth')
+    plt.show()
 
-        #create classifier object
-        clf = RandomForestClassifier(n_estimators=200, criterion='gini', max_features='auto', bootstrap=True, max_samples=None, max_depth=4)
-        #train classifier object
-        clf.fit(X_train, y_train)
-        #test classifier
-        y_pred = clf.predict(X_test)
-        accuracy = ev.overallAccuracy(y_test, y_pred)
-
-        
-        test_size_record.append(test_size)
-        AO.append(accuracy)
-
-    for size in test_size_record:
-        test_size_neg.append(1-size)
-
-    #X
-    test_size_neg = np.array(test_size_neg)
-    #Y
-    AO = np.array(AO)
-
-    #second order log fit line to data
-    log_fit = np.polyfit(np.log(test_size_neg), AO, 2)
-    y_line = log_fit[0] * np.log(test_size_neg) ** 2 + log_fit[1] * np.log(test_size_neg) + log_fit[2]
-
-    plt.scatter(test_size_neg, AO)
-    plt.plot(test_size_neg, y_line, 'r')
-    plt.xlabel('Percentage Training Data')
-    plt.ylabel('Overall Accuracy')
+def rf_Plot_n_estiamators(x_train, x_test, y_train, y_test):
+    n_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+    train_results = []
+    test_results = []
+    for estimator in n_estimators:
+        rf = RandomForestClassifier(n_estimators=estimator, n_jobs=-1)
+        rf.fit(x_train, y_train)
+        train_pred = rf.predict(x_train)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train, train_pred)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+        train_results.append(roc_auc)
+        y_pred = rf.predict(x_test)
+        false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
+        roc_auc = auc(false_positive_rate, true_positive_rate)
+        test_results.append(roc_auc)
+    from matplotlib.legend_handler import HandlerLine2D
+    line1, = plt.plot(n_estimators, train_results, 'b', label="Train AUC")
+    line2, = plt.plot(n_estimators, test_results, 'r', label="Test AUC")
+    plt.legend(handler_map={line1: HandlerLine2D(numpoints=2)})
+    plt.ylabel('AUC score')
+    plt.xlabel('n_estimators')
     plt.show()
