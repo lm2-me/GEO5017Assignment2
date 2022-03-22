bab9efa38cde98d5e75c8369b213f257112e3ca7
yuyut <33997318+yuyut@users.noreply.github.com>
2022-03-22 21:35:15 +0100

    errors for implementing the plotting

--- A2.py
+++ A2.py
@@ -10,6 +10,9 @@ import Features as ft
 import Evaluation as ev
 import SVM
 import RF
+# import preprocessing from sklearn
+from sklearn import preprocessing
+
 
 
 # ### import csv file to read features, remove before sumbitting
@@ -42,10 +45,19 @@ if __name__ == "__main__":
     ##dataload = np.loadtxt('features_norm.csv', delimiter=',')
 
     ## = dataload[:,1:]
-    
+    #labeling the data
+    labelen = preprocessing.LabelEncoder()
+    labelen = labelen.fit_transform(y_true)
+    # onehot = preprocessing.OneHotEncoder()
+    # label_onehot = onehot.fit_transform(labelen.reshape(-1,1))
+
+
     #####
     X_train, X_test, y_train, y_test = RF.splitdata(features_only, y_true)
     y_predRF, y_testRF = RF.randomforest(X_train, X_test, y_train, y_test)
+    RF.rf_Plot_max_depth(X_train, X_test, y_train, y_test)
+    RF.rf_Plot_n_estiamators(X_train, X_test, y_train, y_test)
+
 
     rf_oa = ev.overallAccuracy(y_testRF, y_predRF)
     rf_mpa = ev.meanPerClassAccuracy(y_testRF, y_predRF)
