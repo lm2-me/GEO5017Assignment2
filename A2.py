cc5efbca60343ad328ddbde63292547d5bdc7b38
lm2-me <52729991+lm2-me@users.noreply.github.com>
2022-03-23 17:50:00 +0100

    clean up

--- A2.py
+++ A2.py
@@ -13,29 +13,13 @@ import RF
 # import preprocessing from sklearn
 from sklearn import preprocessing
 
-
-
-# ### import csv file to read features, remove before sumbitting
-
-# #pointcloudsdummy = np.random.randn(800).reshape((100,8))
-# #pointcloudsdummy = np.random.randint(0,500,3000).reshape((500,6))
-
+#save features to file
 # cwd = os.getcwd()
-
 # print(cwd)
-
 # # np.savetxt('features_norm.csv', normalized_object_features, delimiter=',')
-# dataload = np.loadtxt('features_norm.csv', delimiter=',')
-
-# features_only = dataload[:,1:]
-# #print(len(features_only))
-# #print(features_only)
-
-
 
 #Main
 if __name__ == "__main__":
-    ##add back for final code before submitting
     y_true = np.loadtxt('y_true.csv', dtype='str', delimiter='\n')
     object_features = np.loadtxt('features_norm_new.csv', delimiter=',')
 
@@ -49,6 +33,7 @@ if __name__ == "__main__":
     # pointCloudDirectory = ft.importFiles()
     # object_features = ft.allObjectProperties(pointCloudDirectory)
 
+    ## Delete unused features at end
     #Average Height, Squareness [2, 6]
     #object_features1 = object_features[:,[2,6]]
     #Height, Average Height, Squareness [0, 2, 6]
